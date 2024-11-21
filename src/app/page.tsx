98b3faffcd51936c5462f64b7f472423d17fdcad
gilbertoloboac <loboacre@gmail.com>
2024-11-20 21:34:25 -0500

    firts commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,101 +1,39 @@
+import CardPost from "@/components/CardPost/CardPost";
+import Header from "@/components/Header/Header";
 import Image from "next/image";
+
 
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
+    <div className="w-full max-w-[1120px] flex flex-col mx-auto mt-12 pb-5">  
+      <Header/>
+      <div className="w-full flex-1 flex gap-8 items-center justify-center mt-10 mb-10">
+        <div className="flex flex-1 h-[334px] relative rounded-2xl overflow-hidden">
+         <Image
+         src="https://images.unsplash.com/photo-1518933165971-611dbc9c412d?q=80&w=1332&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+         alt=""
+         fill={true}
+         style={{objectFit: 'cover'}}
         />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              src/app/page.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
-
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
         </div>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+        <div className="flex flex-1 h-[334px] flex-col gap-6">
+          <h1 className="font-bold text-[40px] text-blue-600">Como desenvolverum blog com NextJS</h1>
+          <p className="text-zinc-600">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quasi voluptatem in, ipsa natus possimus facilis deleniti? Atque, rerum cupiditate nemo voluptatibus harum enim impedit culpa fugiat beatae officia, inventore qui...</p>
+          <div>
+          <p className="font-bold text-zinc-900">Nome do Autor</p>
+          <p className="text-zinc-600 text-sm">20 de novembro de 2024</p>
+        </div>
+        </div>
+      </div>
+      <div className="grid grid-cols-3">
+      <CardPost/>
+      <CardPost/>
+      <CardPost/>
+      <CardPost/>
+      <CardPost/>
+      <CardPost/>
+      </div>
+     
     </div>
   );
 }
---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,7 @@
+export default function Header() {
+    return(
+        <header>
+            <h1 className="font-black text-3xl text-blue-600">BlogTech</h1>
+        </header>
+    )
+}
